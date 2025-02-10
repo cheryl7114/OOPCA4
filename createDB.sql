23b55f86a3f0c4556b0832dc90f9407e25f1aec5
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-02-10 16:57:40 +0000

    sql file to create database

---
+++ createDB.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS 'expenseTracker_CA4';
+CREATE DATABASE 'expenseTracker_CA4';
+USE 'expenseTracker_CA4';
+DROP TABLE IF EXISTS 'expense';
+DROP TABLE IF EXISTS 'income';
+
+CREATE TABLE Expense (
+    expenseID INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    amount DOUBLE NOT NULL,
+    dateIncurred DATE NOT NULL
+);
+
+CREATE TABLE Income (
+    incomeID INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    amount DOUBLE NOT NULL,
+    dateEarned DATE NOT NULL
+);
