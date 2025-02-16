84a277e1a8e0927ecb8b0f20655c060bc47ca03e
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-02-16 19:00:13 +0000

    create sample data

--- createDB.sql
+++ createDB.sql
@@ -19,3 +19,18 @@ CREATE TABLE Income (
     amount DOUBLE NOT NULL,
     dateEarned DATE NOT NULL
 );
+
+INSERT INTO Expense (title, category, amount, dateIncurred) VALUES
+('Groceries at Tesco', 'Food', 120.5, '2024-12-05'),
+('Electricity bill', 'Utilities', 65.75, '2025-12-15'),
+('Netflix subscription', 'Entertainment', 15.99, '2025-01-15'),
+('Lunch at KFC', 'Food', 50, '2025-01-18'),
+('Trip to Belfast', 'Travel', 107.25, '2025-02-03'),
+('Gym membership', 'Health', 40, '2025-02-10'),
+('Valentines date in Dublin', 'Entertainment', 56.8, '2025-02-14');
+
+INSERT INTO Income (title, amount, dateEarned) VALUES
+('Salary', 2500, '2024-12-01'),
+('Bonus', 300.00, '2025-01-01'),
+('Babysitting for Aunt Marie', 100, '2025-01-08'),
+('Freelance website project', 500, '2025-02-12');
